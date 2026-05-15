05af4c075db4e5fa5501abdec9db1c60b032f511
Damian H <damianholona074@gmail.com>
2026-05-15 14:38:05 +0200

    [Task1] add 2 GET endpoints (get page, get by id)

--- product-service/adapter/src/main/java/pl/javakurs/dname074/product_service/ProductController.java
+++ product-service/adapter/src/main/java/pl/javakurs/dname074/product_service/ProductController.java
@@ -1,20 +1,53 @@
 package pl.javakurs.dname074.product_service;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.javakurs.dname074.domain.ProductServiceProvider;
+import pl.javakurs.dname074.dto.CreateProductCommand;
+import pl.javakurs.dname074.dto.PageDto;
+import pl.javakurs.dname074.dto.ProductDto;
 import pl.javakurs.dname074.model.Product;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductController {
-    private ProductServiceProvider productService;
+    private final ProductServiceProvider productService;
+    private final ProductMapper productMapper;
+    private final PageMapper pageMapper;
     // add, delete, update, products page, available configurations
 
     @GetMapping
-    public Page<Product> getProductsPage(Pageable pageable) {
-        return productService.getProductsPage(pageable);
+    public PageDto<ProductDto> getProductsPage(@RequestParam Integer page, @RequestParam Integer size) {
+        PageDto<ProductDto> productPage = pageMapper.toDto(
+                productService.getProductsPage(page, size),
+                productMapper::pojoToDto
+        );
+        return productPage;
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return productService.getProduct(id);
+    }
+
+    @PostMapping
+    public Product addProduct(CreateProductCommand product) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Product modifyProduct(@PathVariable Long id, CreateProductCommand product) {
+        return null;
+    }
+
+    @PatchMapping("/{productId}/configurations/{configurationId}")
+    public Product addConfigurationToProduct(@PathVariable Long productId, @PathVariable Long configurationId) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Product removeProduct(@PathVariable Long id) {
+        return null;
     }
 }
