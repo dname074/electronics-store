2ba6ae8977abb798f093ed473b765b6b84abdd7c
Damian H <damianholona074@gmail.com>
2026-05-15 14:36:21 +0200

    [Task1] add mappers

---
+++ product-service/adapter/src/main/java/pl/javakurs/dname074/product_service/ConfigurationMapper.java
@@ -0,0 +1,15 @@
+package pl.javakurs.dname074.product_service;
+
+import org.mapstruct.Mapper;
+import pl.javakurs.dname074.dto.ConfigurationDto;
+import pl.javakurs.dname074.dto.CreateConfigurationCommand;
+import pl.javakurs.dname074.model.Configuration;
+import pl.javakurs.dname074.product_service.entity.ConfigurationEntity;
+
+@Mapper(componentModel = "spring")
+public interface ConfigurationMapper {
+    ConfigurationDto toDto(Configuration configuration);
+    Configuration entityToPojo(ConfigurationEntity configuration);
+    Configuration dtoToPojo(CreateConfigurationCommand configuration);
+    ConfigurationEntity toEntity(Configuration configuration);
+}
---
+++ product-service/adapter/src/main/java/pl/javakurs/dname074/product_service/ProductMapper.java
@@ -0,0 +1,20 @@
+package pl.javakurs.dname074.product_service;
+
+import org.mapstruct.Mapper;
+import pl.javakurs.dname074.dto.CreateProductCommand;
+import pl.javakurs.dname074.dto.ProductDto;
+import pl.javakurs.dname074.model.Product;
+import pl.javakurs.dname074.model.ProductConfiguration;
+import pl.javakurs.dname074.product_service.entity.ProductConfigurationEntity;
+import pl.javakurs.dname074.product_service.entity.ProductEntity;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    ProductDto pojoToDto(Product product);
+    Product entityToPojo(ProductEntity product);
+    Product dtoToPojo(CreateProductCommand product);
+    ProductEntity pojoToEntity(Product product);
+
+    ProductConfiguration productConfigurationToPojo(ProductConfigurationEntity value);
+    ProductConfigurationEntity productConfigurationToEntity(ProductConfiguration value);
+}
