135c7dc4fa03c0eb00e0efc5018aae34cb302f40
Damian H <damianholona074@gmail.com>
2026-05-24 16:00:06 +0200

    [Task2] + cart-service module, poms

---
+++ cart-service/cart-adapter/src/main/java/pl/javakurs/dname074/cart_service/CartServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.javakurs.dname074.cart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApplication.class, args);
+	}
+
+}
---
+++ cart-service/cart-model/src/main/java/pl/javakurs/dname074/cart/model/CartProduct.java
@@ -0,0 +1,4 @@
+package pl.javakurs.dname074.cart.model;
+
+public class CartProduct {
+}
